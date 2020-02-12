65cd7afc12b68b6af3be0d3a16c51734e9ad7e68
Tung <tungbtruong@gmail.com>
2020-02-12 13:54:11 -0500

    Restructured P3

--- PS2/PS2.P3.js
+++ PS2/PS2.P3.js
@@ -4,9 +4,6 @@ code to call the function on each value of the array [1,2,3,4,5,6,7].
 Note: The .map() method on Array is your friend here
 */
 
-const cube = (x) => {
-    return x**3;
-}
 
-let newNumbs = [1,2,3,4,5,6,7].map(cube);
+let newNumbs = [1,2,3,4,5,6,7].map(x => x**3);
 console.log(newNumbs);
