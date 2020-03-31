9ddccfe05356313725226261a544e80cb01a35a4
Tung <tungbtruong@gmail.com>
2020-03-30 18:52:12 -0600

    Finalized Weather Call

--- PS4/routes/ps4.js
+++ PS4/routes/ps4.js
@@ -8,7 +8,11 @@ router.get('/', async (req, res, next) => {
     const api_url = "https://api.darksky.net/forecast/380fbd76b13314d731a314ee13b83c70/42.3601,71.0589"
     const fetch_response = await fetch(api_url);
     const json = await fetch_response.json()
-    res.json(json);
+    res.render("WeatherRaw", {text: JSON.stringify(json)})
 });
 
 module.exports = router;
+
+/*
+res.render("probA", {test: "hello"})
+*/
