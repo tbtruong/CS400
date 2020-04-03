884e3e80c79b052d2dca08a1a7c22a706eac2835
Tung <tungbtruong@gmail.com>
2020-04-02 19:04:06 -0600

    Finalized PS4

--- PS4/routes/ps4.js
+++ PS4/routes/ps4.js
@@ -6,13 +6,10 @@ const fetch = require('node-fetch');
 /* GET home page. */
 router.get('/', async (req, res, next) => {
     const api_url = "https://api.darksky.net/forecast/380fbd76b13314d731a314ee13b83c70/42.3601,71.0589"
-    const fetch_response = await fetch(api_url);
-    const json = await fetch_response.json()
+    const data = await fetch(api_url);
+    const json = await data.json()
     res.render("WeatherRaw", {text: JSON.stringify(json)})
 });
 
 module.exports = router;
 
-/*
-res.render("probA", {test: "hello"})
-*/
